8276013aaaf8cd55cad8696d47f7497a73ace7b3
Pranoy T  Murali <71282341+PranoyTM@users.noreply.github.com>
2024-12-24 17:00:12 +0530

    Add files via upload

---
+++ Jobworkflow6.py
@@ -0,0 +1,12 @@
+# Example Python script for Script Executor component
+def execute_script():
+    num1 = 10
+    num2 = 20
+# Perform the addition
+    result = num1 + num2
+
+# Print the resulta
+    print(f"sum of {num1} and {num2} is {result}")
+# Ensure that the script is executed when the file is run
+if __name__ == "__main__":
+    execute_script()
